036ab7f77bac54ba45b5ccb9a821f556f2d9719b
dasar <wanjikuestivah@gmail.com>
2023-09-14 21:34:15 +0300

    grid without slider

--- script.js
+++ script.js
@@ -6,24 +6,7 @@ buildGrid(selectedSize);
 //const colorPicker = document.querySelector('#inputcolor');
 const colorButtons = document.querySelectorAll('.color-choice');
 
-console.log(colorButtons)
-//draggable constractors creates the ability rtt rounded controllers
-/* const sizeController = Draggable.create("#size-controller",{
-    type: "rotation",
-    bounds:{minRotation:90, maxRotation: 180},
-    ondragEnd: () => {
-        sizeController[0].endRotation > 135 ?selectedSize = 32 : selectedSize = 16;
-        eraseGrid();
-    }
-});
-const colorController = Draggable.create("#color-controller",{
-    type: "rotation",
-    bounds:{minRotation:0, maxRotation: 90},
-    ondragEnd: ()=> {
-        console.log('color controller on drag event event invoked')
-        colorController[0].endRotation < 45 ? selectedColor = 'black' : selectedColor = 'random';
-    }
-});  */
+ 
 
 
 function buildGrid(size) {
@@ -49,7 +32,6 @@ function buildGrid(size) {
 }
 //painting the grid
 function paintGrid(elem, color){
-    console.log('invoking the paintgrid function')
     if(elem.buttons == 1){
         if(elem.target.classList == 'square-grid'){
             let square =elem.target;
@@ -68,7 +50,6 @@ function eraseGrid(){
 }
 //returning0f random rgb colr
 function getRandomRgb(){
-    console.log('random rgb function started')
     var num = Math.round(0xffffff * Math.random());
     var r = num >> 16;
     var g = num >> 8 &  255;
@@ -77,13 +58,10 @@ function getRandomRgb(){
 }
 //paint when mouse event is selectedColor
 grid.addEventListener('mousedown', Event =>{
-    console.log('mouse down event detected')
     paintGridEvent = paintGrid(Event, selectedColor);
     console.log(Event.buttons)
     if(Event.buttons == 1){
-        console.log('Event.buttons == 1')
         window.addEventListener('mouseover', (e) =>{
-            console.log(selectedColor)
             if(selectedColor == 'random'){
                 paintGrid(e, getRandomRgb());
             }else{
@@ -96,20 +74,14 @@ grid.addEventListener('mousedown', Event =>{
 
 //clean the grid event
 eraseButton.addEventListener('click', () =>{
-    console.log("bla")
     eraseGrid();
-    //selectedColor = 'random';
-    //console.log(selectedColor);
+   
 });
 
  function changecolor(event) {
-    console.log('i was here')
-    console.log(event.target.dataset.color)
     switch (event.target.dataset.color) {
         case 'random':
-        console.log("was here too")
         selectedColor = 'random';
-        console.log(selectedColor)
         break;
         case 'black':
         selectedColor = 'black';
@@ -119,9 +91,6 @@ eraseButton.addEventListener('click', () =>{
  
  }
 
-/*  function userColorSelection(event) {
-    color = event.target.value;
-} */
 
 function buttonHover() {
     this.style.border = '1px solid #FF0000';
@@ -133,6 +102,3 @@ function buttonStandard() {
 colorButtons.forEach(colorButtons => colorButtons.addEventListener('click', changecolor));
 colorButtons.forEach(colorButton => colorButton.addEventListener('mouseover', buttonHover));
 colorButtons.forEach(colorButton => colorButton.addEventListener('mouseout', buttonStandard));
-
-//colorPicker.addEventListener('change', userColorSelection, false);
-//colorPicker.addEventListener('input', userColorSelection, false);
